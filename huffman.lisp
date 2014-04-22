9aa4e60387592de62d284f16eefd88b807b5886f
Krody Robert <robi@presslabs.com>
2014-04-22 15:35:06 +0300

    Added generation of huffman codes

--- huffman.lisp
+++ huffman.lisp
@@ -21,6 +21,106 @@
 )
 
 ;;; params
+;;; 	hash table of occurence of chars
+;;; objective
+;;;		generate a list of leaf nodes
+;;; return
+;;;		list
+(defun init-huff-list (table)
+	(let ((lst '()))
+		(MAPHASH #'(lambda (k v) (setq lst (cons (list v k) lst))) table)
+		lst
+	)
+)
+
+;;; params
+;;; 	list of leaf nodes
+;;; objective
+;;;		generate a huff tree
+;;; return
+;;;		huff tree
+(defun huff-tree (lst)
+	(do ((tree lst)
+		(left nil)
+		(right nil))
+
+		((<= (length tree) 1) (car tree))
+
+		(setq tree (my-node-sort tree))
+		(setq left (car tree))
+		(setq right (cadr tree))
+		(setq tree (cons (node-merge left right) (cddr tree)))
+	)
+)
+
+
+;;; params
+;;; 	a huffman tree
+;;; objective
+;;;		generate a huff tree hash table
+;;; return
+;;; 	table key => char value => code
+(defun huff-code-hash (tree)
+	(let ((table (make-hash-table)))
+		
+		;; local function node-haser
+		;; it parses each child and determines if it is a leaf constructing its binary code in the process
+		(labels ((node-hasher (node prev-code)
+			(let ((left (left-child node))
+				  (right (right-child node)))
+
+				(if left
+					(if (leaf? left)
+						(setf (gethash (cadr left) table) (concatenate 'string prev-code "0"))
+						(node-hasher left (concatenate 'string prev-code "0"))))
+
+				(if right
+					(if (leaf? right)
+						(setf (gethash (cadr right) table) (concatenate 'string prev-code "1"))
+						(node-hasher right (concatenate 'string prev-code "1"))))
+			)))
+
+			;; body of label
+			;; initial check if tree exists and or if it is made out of only a leaf
+			(if tree
+				(if (leaf? tree)
+					(setf (gethash (cadr tree) table) "0")
+					(progn
+						(if (leaf? (left-child tree))
+							(setf (gethash (cadr (left-child tree)) table) "0")
+							(node-hasher (left-child tree) "0"))
+
+						(if (leaf? (right-child tree))
+							(setf (gethash (cadr (right-child tree)) table) "1")
+							(node-hasher (right-child tree) "1"))
+					)
+				)
+			)
+	
+			;; return the hash tabel
+			table
+		)
+	)
+)
+
+
+
+
+;;;;;;; HELPER FUNCTIONS FOR TREE MANIPULATION
+
+;;; params
+;;; 	first node
+;;; 	second node
+;;; objective
+;;;		create a new node with total weight of both of its children
+;;; return
+;;;		merged node
+(defun node-merge (left right)
+	(list (+ (weight left) (weight right)) left right)
+)
+
+
+;;; params
 ;;; 	first node
 ;;; 	second node
 ;;; objective
@@ -34,21 +134,6 @@
 	)
 )
 
-
-;;;;;;; HELPER FUNCTIONS FOR TREE MANUPULATION
-
-;;; params
-;;; 	hash table of occurence of chars
-;;; objective
-;;;		generate a list of leaf nodes
-;;; return
-;;;		list
-(defun init-huff-list (table)
-	(let ((lst '()))
-		(MAPHASH #'(lambda (k v) (setq lst (cons (list v k) lst))) table)
-		lst
-	)
-)
 
 ;;; params
 ;;; 	list of nodes
@@ -82,7 +167,10 @@
 ;;; return
 ;;;		numeric weight
 (defun weight (node)
-	(car node)
+	(if (car node)
+		(car node)
+		0
+	)
 )
 
 
@@ -132,15 +220,17 @@
 
 
 
-;;;;;; TEST node smaller, init function and huffman sort
+;;;;;; TEST node smaller, init function ,huffman sort ,huffman tree generation
 ;(print (node-smaler? '(1 a) '(2 b)))
 ;(print (node-smaler? '(10 a) '(2 b)))
 ;(print (init-huff-list (parse-input "C:\\Users\\Robi\\Desktop\\test_huf_in.txt")))
 ;(print (my-node-sort (init-huff-list (parse-input "C:\\Users\\Robi\\Desktop\\test_huf_in.txt"))))
+;(print (huff-tree (init-huff-list (parse-input "C:\\Users\\Robi\\Desktop\\test_huf_in.txt"))))
 ;;;;;; END test set
 
 
 
 ;;;;;; TEST hash table creation
 ;(MAPHASH #'(lambda (k v) (print (list k v))) (parse-input "C:\\Users\\Robi\\Desktop\\test_huf_in.txt"))
+(MAPHASH #'(lambda (k v) (print (list k v))) (huff-code-hash (huff-tree (init-huff-list (parse-input "C:\\Users\\Robi\\Desktop\\test_huf_in.txt")))))
 ;;;;;; END test set
